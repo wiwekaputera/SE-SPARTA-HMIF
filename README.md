add2800d20e9b4f339d072e56170da98912b5926
I Made Wiweka Putera <78787384+wiwekaputera@users.noreply.github.com>
2024-08-10 23:33:17 +0700

    Final submission

--- README.md
+++ README.md
@@ -1,36 +1,38 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+Ini adalah projek Hands On Software Engineering SPARTA HMIF 2023 menggunakan [Next.js](https://nextjs.org/) yang dibuat dengan [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-## Getting Started
+## Teknologi yang Digunakan
 
-First, run the development server:
+- [Next.js](https://nextjs.org/) - Framework React untuk pengembangan aplikasi web.
+- [Tailwind CSS](https://tailwindcss.com/) - Framework CSS untuk styling yang cepat dan responsif.
+- [Aceternity UI](https://aceternity-ui.com/) - Komponen UI untuk pengembangan antarmuka pengguna.
+- [TypeScript](https://www.typescriptlang.org/) - Bahasa pemrograman yang diketik secara statis untuk pengembangan JavaScript.
+
+## Memulai
+
+Pertama, jalankan server pengembangan:
 
 ```bash
 npm run dev
-# or
+# atau
 yarn dev
-# or
+# atau
 pnpm dev
-# or
+# atau
 bun dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Buka [http://localhost:3000](http://localhost:3000) dengan browser Anda untuk melihat hasilnya.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## Bonus yang dikerjakan
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+Bonus yang dikerjakan adalah tampilan yang responsif agar terlihat baik di berbagai device.
 
-## Learn More
+## Kesulitan yang dihadapi
 
-To learn more about Next.js, take a look at the following resources:
+Kesulitan yang dihadapi adalah proses belajar tools baru yang dipakai pada projek ini.
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+## Identitas
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Nama: I Made Wiweka Putera
+NIM: 19623279
+Klan: T
---
+++ components/Hero.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
+import { SocialIcon } from 'react-social-icons'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36 min-h-screen flex flex-col justify-center items-center'>
+        <div>
+            <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
+            <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />
+            <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue' />
+        </div>
+
+        <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        {/* Radial gradient for the container to give a faded look */}
+            <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]">
+            </div>
+        </div>
+
+        <div className='flex justify-center relative my-20 z-10'>
+            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col md:flex-row items-center justify-center'>
+                <img 
+                    src="/foto jamal.jpg" 
+                    alt="My Photo" 
+                    className="w-48 h-48 sm:w-64 sm:h-64 md:w-56 md:h-64 lg:w-64 lg:h-72 rounded-full object-cover hover:scale-110 transition-transform duration-300 md:mr-8 mb-4 md:mb-0 md:-mt-28" 
+                />
+                <div className='flex flex-col items-center md:items-start justify-center relative md:-mt-2'>
+                    <h2 className='tracking-widest text-md text-center md:text-left text-blue-100 max-w-80 hover:scale-110 transition-transform duration-300'>
+                        Hi! I&apos;m
+                    </h2>
+
+                    <TextGenerateEffect words='I Made Wiweka' className='md:text-5xl lg:text-6xl text-[40px] text-center md:text-left hover:scale-110 transition-transform duration-300 md:whitespace-nowrap' />
+
+                    <p className='text-center md:text-left md:tracking-wider mb-4 text-xs md:text-lg hover:scale-110 transition-transform duration-300'>
+                        a student of Informatics Engineering at Institut Teknologi Bandung. I&apos;m interested in web development and I&apos;m currently learning Next.js.
+                    </p>
+
+                    <div className='flex flex-row items-center space-x-4'>
+                        <a href="https://github.com/wiwekaputera" target="_blank" rel="noopener noreferrer">
+                            <MagicButton 
+                                title='My Work'
+                                icon={<FaLocationArrow />}
+                                position='right'
+                            />
+                        </a>
+                        <SocialIcon network="linkedin" className='md:mt-10 sm:mt-4' target="_blank" url='https://www.linkedin.com/in/imadewiwekaputera/'/>
+                        <SocialIcon network="instagram" className='md:mt-10 sm:mt-4' target="_blank" url='https://www.instagram.com/wiwekaputera'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
